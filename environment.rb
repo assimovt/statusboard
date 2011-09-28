41c4d0584f1ed3fd2bae108c89f80442d0921af3
Tair Assimov <tair@assimov.net>
2011-09-28 23:58:27 +0300

    Add app settings yml

--- environment.rb
+++ environment.rb
@@ -11,15 +11,11 @@ require 'ostruct'
 require 'sinatra' unless defined?(Sinatra)
 
 configure do
-  SiteConfig = OpenStruct.new(
-                 :title => 'Your Application Name',
-                 :author => 'Your Name',
-                 :url_base => 'http://localhost:4567/'
-               )
-
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
 
   DataMapper.setup(:default, (ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
 end
+
+APP_CONFIG = YAML.load_file("#{File.dirname(__FILE__)}/status.yml")[Sinatra::Base.environment.to_s]
