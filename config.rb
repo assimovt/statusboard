03c85c3e74f1b1204a50fd0e980fa9327e0336e5
dolgobro <maximd@solar.local>
2011-10-12 07:24:54 +0300

    Added new files

---
+++ config.rb
@@ -0,0 +1,35 @@
+require './application'
+
+# Compass Configuration
+
+#require 'grid-coordinates'
+#require 'meyer-reset'
+
+# Configuration to use when running within Sinatra
+project_path          = Sinatra::Application.root
+
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/stylesheets'
+http_images_path      = '/images'
+http_javascripts_path = '/javascripts'
+
+# File system locations
+css_dir               = File.join 'public', 'stylesheets'
+sass_dir              = File.join 'views', 'stylesheets'
+images_dir            = File.join 'public', 'images'
+javascripts_dir       = File.join 'public', 'javascripts'
+
+# Syntax preference
+preferred_syntax      = :sass
+
+# Determine whether Compass generates relative or absolute paths
+relative_assets       = false
+
+# Determines whether line comments should be added to compiled css for easier debugging
+line_comments         = false
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :expanded
+
+# Learn more: http://beta.compass-style.org/help/tutorials/configuration-reference/
