d4a842a3fb196b65ca420fd0edd600e490c4d98d
dolgobro <maximd@solar.local>
2011-10-12 12:46:29 +0300

    Overall status is updated based on nodes statuses

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,9 @@
 StatusBoard = (function($) {
   var refreshRate = 1000,
-      serviceStatusEl = '#service-status'
+      serviceStatusEl = '#service-status',
+      lastUpdateEl = '#last-updated',
       statusTmpl = '<div class="node"><span class="name">{{uri}}</span><span class="node-status"><span class="status {{status}}"></span></span></div>',
-      uptimeTmpl = '<div class="node"><span class="name">{{uri}}</span><span class="node-uptime"><span class="uptime {{uptime_class}}">{{uptime}}</span></span></div>',
+      uptimeTmpl = '<div class="node"><span class="name">{{uri}}</span><span class="node-status wide"><span class="progress" style="width: {{uptime}}px;"></span><span class="uptime">{{uptime}}%</span></span></div>',
       timer = '';
   
   var updateTime = function() {
@@ -18,7 +19,7 @@ StatusBoard = (function($) {
     }else{
       stopInt();
     }
-  }
+  };
 
   var stopInt = function(func) {
     if(timer !== "") {
@@ -26,29 +27,49 @@ StatusBoard = (function($) {
       timer = "";
       console.log("Interval stopped");
     }
-  }
+  };
   
   var getStatus = function(status) {
     return (status) ? 'up':'down'; 
-  }
+  };
 
+  var updateStatuses = function(statuses) {
+    $.each(statuses, function(key, value){
+      var status = $("#service-status ." + key+"-status").find('.status');
+      status.html(value);
+      if(value > 0) {
+        status.addClass(key);
+      } else {
+        status.removeClass(key);
+      }
+    });
+  };
+  
   var showStatus = function() {
-    var output = "";
-    var obj = jQuery.parseJSON('[{"timestamp":"12:00", "status":true, "uri":"server 1"}, {"timestamp":"13:00", "status":true, "uri":"server 2"}]');
-    $(serviceStatusEl).show(); // if is hidden
+    var output = "", statuses = { up:0, down:0};
+    var obj = jQuery.parseJSON('[{"timestamp":"12:00", "status":false, "uri":"server 3"}, {"timestamp":"12:00", "status":true, "uri":"server 1"}, {"timestamp":"13:00", "status":true, "uri":"server 2"}]');
+    $(serviceStatusEl).slideDown();
+    $(lastUpdateEl).fadeIn();
     //$.getJSON(url+query,function(json){});
     
     $.each(obj, function(i, node) {
       output += Mustache.to_html(statusTmpl, {uri: node.uri, status: getStatus(node.status)});
+      if(node.status) {
+        statuses.up += 1;
+      } else {
+        statuses.down += 1;
+      }
     });
-    
-    $("#nodes-status").html(output);
+
+    updateStatuses(statuses);
+    $("#nodes").html(output);
     updateTime();
   };
   
   var showUptime = function(startTime, endTime) {
     var output = "";
-    $(serviceStatusEl).hide(); // if is visible
+    $(serviceStatusEl).slideUp();
+    $(lastUpdateEl).fadeOut();
     var obj = jQuery.parseJSON('[{"timestamp":"12:00", "status":true, "uri":"server 1"}, {"timestamp":"13:00", "status":true, "uri":"server 2"}]');
     
     //startTime = (startTime.length > 0) ? "" : "";
@@ -56,10 +77,10 @@ StatusBoard = (function($) {
     
     $.each(obj, function(i, node) {
       // Request node uptime
-      output += Mustache.to_html(uptimeTmpl, {uri: node.uri, uptime: 80});
+      output += Mustache.to_html(uptimeTmpl, {uri: node.uri, uptime: 3});
     });
     
-    $("#nodes-status").html(output);
+    $("#nodes").html(output);
   };
   
   var showStats = function() {
