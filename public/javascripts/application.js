7cc8c80192dcdd3fe8791446ec076f932041d2e2
tariel <maxim.dolgobrod@gmail.com>
2011-11-02 21:46:19 +0200

    Refactored status view functions. Added periodic polling of status.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,56 +1,16 @@
-var StatusBoard = (function($) {
-  var refreshRate = 2000,
-      serviceStatusContainer = '#service-status',
-      serviceUptimeContainer = '#service-uptime',
-      lastUpdateEl = '#last-updated',
-      uptimePeriodEl = '#uptime-period',
-      downtimeExplanationEl = '#downtime-explanation',
-      nodesContainer = '#nodes',
-      statusTmpl = '<div class="node"><span class="name">{{uri}}</span><span class="node-status"><span class="status {{status}}"></span></span></div>',
-      uptimeTmpl = '<div class="node"><span class="name">{{uri}}</span><span class="node-status wide"><span class="progress" style="width: {{width}}px;"></span><span class="uptime">{{uptime}}%</span></span></div>',
-      timer = '',
-      nodesCount = 0,
-      nodes = [],
-      ajaxRequests = [];
-  
-  var stopInt = function() {
-    if(timer !== "") {
-      window.clearInterval(timer);
-      timer = "";
-    }
-  };
-  
-  var startInt = function(func) {
-    if(timer === "") {
-      timer = window.setInterval(func, refreshRate);
-    }else{
-      stopInt();
-    }
-  };
-  
-  var addRequest = function(request) {
-    ajaxRequests.push(request);
-  };
-  
-  var stopAjaxRequests = function() {
-    $.each(ajaxRequests, function(i, request) {
-      request.abort();
-      $.grep(ajaxRequests, function(value) {
-        return value !== request;
-      });
-    });
-  };
-  
-  var convertToTimestamp = function(year,month,day,hours,minutes,seconds){
+var Utils = {
+  nodesContainer: '#nodes',
+
+  convertToTimestamp: function(year,month,day,hours,minutes,seconds){
     var dateTime = new Date(Date.UTC(year,month-1,day,hours,minutes,seconds));
     return Math.round(dateTime.getTime()/1000);
-  };
+  },
 
-  var getCurrentTimestamp = function() {
+  getCurrentTimestamp: function() {
     return Math.round((new Date()).getTime() / 1000);
-  };
+  },
   
-  var getDateTime = function(format, timestamp) {
+  getDateTime: function(format, timestamp) {
     var output = "";
     var dt = {};
     var dateTime = (timestamp === 0) ? new Date() : new Date(timestamp*1000);
@@ -83,261 +43,23 @@ var StatusBoard = (function($) {
     }
     
     return output;
-  };
+  },
   
-  var updateTime = function() {
-    $(lastUpdateEl).find('.datetime').html(getDateTime('', 0));
-  };
-  
-  var getStatus = function(status) {
-    return (status) ? 'up':'down'; 
-  };
-
-  var showOverallStatus = function(statuses) {
-    var statusText = $('#app-name').val(),
-        statusClass = '';
-        
-    if(statuses.down === nodes.length && statuses.up === 0) {
-      statusText += ' is down';
-      statusClass = 'service-down';
-      $(downtimeExplanationEl).slideDown('fast');
-    } else {
-      statusText += ' is up';
-      statusClass = 'service-up';
-      $(downtimeExplanationEl).hide();
-    }
-    
-    $(serviceStatusContainer).addClass(statusClass).find('h1').html(statusText);
-  };
-  
-  var countStatuses = function(statuses, status) {
-    if(status) {
-      statuses.up++;
-    } else {
-      statuses.down++;
-    }
-    
-    return statuses;
-  };
-  
-  var showError = function(msg) {
+  showError: function(msg) {
     $(nodesContainer).html('<div class="loader">'+msg+'</div>');
-  };
+  },
   
   // Show loader when ajaxRequest is made
-  var showLoader = function() {
+  showLoader: function() {
     $(nodesContainer).ajaxStart(function() {
-      $(nodesContainer).html('<div class="loader">Loading..</div>');
+     // $(nodesContainer).html('<div class="loader">Loading..</div>');
     });
-  };
-  
-  // Save list of existing nodes
-  var createNodesList = function(node) {
-    if(nodes.length < nodesCount) {
-      nodes.push(node);
-    }
-  };
-  
-  // Initialize datePicker for uptime period selection
-  var enableDatePicker = function() {    
-    // Set default dates
-    if($('#from').val().length === 0 && $('#to').val().length === 0) {
-      var dateTime = new Date();
-      var year = dateTime.getFullYear();
-      var startMonth = dateTime.getMonth();
-      var endMonth = dateTime.getMonth()+1;
-      var day = dateTime.getDate();
-      var startDate = day+"."+startMonth+"."+year;
-      var endDate = day+"."+endMonth+"."+year;
-      
-      $('#from').val(startDate);
-      $('#to').val(endDate);
-      
-      $('#actualdate-from').val(convertToTimestamp(year, startMonth, day, 0, 0, 0));
-      $('#actualdate-to').val(convertToTimestamp(year, endMonth, day, 0, 0, 0));
-    }
-    
-    // Enable datepicker
-    $.datepicker.setDefaults({
-      dateFormat: 'dd.mm.yy',
-      altFormat: '@',
-      changeMonth: true,
-      showOn: "button",
-      buttonImage: "images/calendar.gif",
-      buttonImageOnly: true,
-      buttonText: 'Choose date'});
-  
-    var dateFrom = $('#from').datepicker({
-      altField: '#actualdate-from',
-      onSelect: function(selectedDate) {
-        dateTo.datepicker('option', 'minDate', dateFrom.datepicker('getDate'));
-      }
-    });
-        
-    var dateTo = $('#to').datepicker({
-      altField: '#actualdate-to',
-      onSelect: function(selectedDate) {
-        dateFrom.datepicker('option', 'maxDate', dateTo.datepicker('getDate'));
-      }
-    });
-  };
-  
-  var getUptimePeriodStartDate = function() {
-    var date = $('#actualdate-from').val();
-    
-    // Fix date format to Unix timestamp
-    if(date.length === 13) {
-      date = date/1000;
-    }
-    
-    return date;
-  };
-  
-  var getUptimePeriodEndDate = function() {
-    var date = $('#actualdate-to').val();
-    
-    // Fix date format to Unix timestamp
-    if(date.length === 13) {
-      date = date/1000;
-    }
-    
-    date = parseInt(date, 10) + (60*60*23 + 59*60 +59);
-    
-    return date;
-  };
-  
-  // Display service status
-  var showStatus = function() {
-    var output = "", statuses = {up:0, down:0};
-    
-    $(serviceUptimeContainer).hide();
-    $(serviceStatusContainer).fadeIn(400, function() {
-      $(uptimePeriodEl).hide();
-      $(lastUpdateEl).fadeIn(400);
-    });
-     
-    function loadData() {
-      var request = $.ajax({
-        url: "statuses.json",
-        type: "GET",
-        dataType: "json"});
-      
-      addRequest(request);
-      
-      request.done(function(data) {
-        $.each(data, function(i, node) {
-          countStatuses(statuses, node.status);
-          output += Mustache.to_html(statusTmpl, {uri: node.uri, status: getStatus(node.status)});
-          nodesCount++;
-          createNodesList(node.uri);
-        });
-        
-        showOverallStatus(statuses);
-        $(nodesContainer).html(output);
-        updateTime();
-        output = "";
-        nodesCount = 0;
-        statuses = {up:0, down:0};
-      });
-      
-      request.fail(function() {
-        showError("Sorry, couldn't load data."); 
-      });
-    
-      $(nodesContainer).ajaxComplete(function() {
-       
-      });
-    }
-    
-    loadData();
-  };
-  
-  // Display service uptime
-  var showUptime = function() {
-    var output = "", loaded = 0;
-    
-    $(serviceStatusContainer).hide();
-    $(downtimeExplanationEl).hide();
-    $(serviceUptimeContainer).fadeIn(400, function() {
-      $(lastUpdateEl).hide();
-      $(uptimePeriodEl).fadeIn(200);
-    });
-      
-    enableDatePicker();
-    var startTime = getUptimePeriodStartDate();
-    var endTime = getUptimePeriodEndDate();
-
-    $.each(nodes, function(i, n) {
-      var request = $.ajax({
-        url: "uptime",
-        type: "GET",
-        data: {start_time: startTime, end_time: endTime, node: n}
-      });
-      addRequest(request);
-      
-      request.done(function(data) {
-        loaded++;
-        
-        if(data.length === 0) {
-          data = 0;
-        }
-        output += Mustache.to_html(uptimeTmpl, {uri: n, uptime: data, width: Math.round(data)});
-      
-        if(loaded === nodes.length) {
-          //$(serviceUptimeContainer).find('.total-uptime > dd').addClass('uptime').html(100 + " %");        
-          $(nodesContainer).html(output);
-          output = "";
-          loaded = 0;
-        }
-      });
-      
-      request.fail(function(data) {
-        showError("Sorry, couldn't load data.");
-      });
-    });
-    
-    $(nodesContainer).ajaxComplete(function() {
-      
-    });
-  };
-  
-  // Switch views
-  var toggleStatus = function() {
-    var activeClass = 'active';
-    showLoader();
-    
-    $('.nav-link').click(function(){
-      var el = $(this);
-      var activeLink = el.attr("href");
-      stopAjaxRequests();
-      
-      // Set active class for main navigation
-      if(el.parents('#filter-options').length > 0) {
-        $('#filter-options li').removeClass(activeClass);
-        el.parent().addClass(activeClass);
-      }
-      
-      // Switch the view based on the selection
-      if(activeLink === '#uptime') {
-        showUptime();
-      } else {
-        showStatus();
-      }
-      
-      return false;
-    });
-    
-    if($('#filter-options li').hasClass(activeClass)) {
-      showStatus();
-    }
-  };
-  
-  return {
-    toggleStatus: toggleStatus
-  };
-})(jQuery);
-
+  }
+};
 
 $(document).ready(function() {
-  //StatusBoard.toggleStatus();
+  if ($('#service-status').length) {
+    StatusBoard.Status.init();
+  }
+  
 });
---
+++ public/javascripts/status.js
@@ -0,0 +1,89 @@
+var StatusBoard = StatusBoard || {};
+
+StatusBoard.Status = {
+  serviceStatusContainer: '#service-status',
+  lastUpdateEl: '#last-updated',
+  downtimeExplanationEl: '#downtime-explanation',
+  nodesContainer: '#nodes',
+  nodeStatusTmpl: '<div class="node"><span class="name">{{uri}}</span><span class="node-status"><span class="status {{status}}"></span></span></div>',
+  serviceUpText: ' is up',
+  serviceDownText: ' is down',
+  failedRequests: 0,
+  updateInterval: 6000,
+  
+  init: function() {
+    this.getStatusData();
+  },
+  
+  updateTime: function() {
+    $(this.lastUpdateEl).find('.datetime').html(Utils.getDateTime('', 0));
+  },
+  
+  getStatusClass: function(status) {
+    return (status) ? 'up':'down'; 
+  },
+
+  setGlobalStatus: function(nodesUp, nodesDown) {
+    var statusText = $('#app-name').val(),
+        statusClass = '';
+     
+    if(nodesDown > 0 && nodesUp === 0) {
+      statusText += this.serviceDownText;
+      statusClass = 'service-down';
+      $(this.downtimeExplanationEl).slideDown('fast');
+    } else {
+      statusText += this.serviceUpText;
+      statusClass = 'service-up';
+      $(this.downtimeExplanationEl).hide();
+    }
+    
+    $(this.serviceStatusContainer).removeClass().addClass(statusClass).find('h1').html(statusText);
+  },
+  
+  getStatusData: function() {
+    var self = this,
+        output = "",
+        nodesUp = 0,
+        nodesDown = 0;
+        
+    var request = $.ajax({
+        url: "statuses.json",
+        type: "GET",
+        dataType: "json"});
+      
+    request.done(function(data) {
+      $.each(data, function(i, node) {
+        if(node.status) {
+          nodesUp++;
+        } else {
+          nodesDown++;
+        }
+        output += Mustache.to_html(self.nodeStatusTmpl, {uri: node.uri, status: self.getStatusClass(node.status)});
+      });
+
+      self.setGlobalStatus(nodesUp, nodesDown);
+      $(self.nodesContainer).html(output);
+      self.updateTime();
+            
+      // Recurse on success
+      setTimeout(function() { self.init(); }, self.updateInterval);
+    });
+    
+    request.fail(function() {
+      //this.errorHandler();
+      $.proxy(this.errorHandler, this);
+      Utils.showError("Sorry, couldn't load data."); 
+    });
+  },
+ 
+   // Handle errors
+  errorHandler: function(){
+    if(++this.failedRequests < 10){
+      // Give the server some breathing room by increasing the updateInterval
+      this.updateInterval += 1000;
+
+      // recurse
+      this.init();
+    }
+  }
+};
